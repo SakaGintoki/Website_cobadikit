48915e0d9b1972fd0ebcd3e9ed230b50dde611b3
malvinshahharisathala <108750245+SakaGintoki@users.noreply.github.com>
2024-01-06 21:15:22 +0700

    Add files via upload

---
+++ Try Some Project/Project 4/script.js
@@ -0,0 +1,12 @@
+
+    const preloader = document.querySelector('.preloader');
+
+  
+    // 👇️ removes element from DOM
+    menuToggle.addEventListener('setTimeout(()=> {}, 1000)', function() {
+            preloader.classList.toggle('hilang');
+    }
+
+
+    // 👇️ hides element (still takes up space on page)
+    // box.style.visibility = 'hidden';  // 👈️ time in millisecond
